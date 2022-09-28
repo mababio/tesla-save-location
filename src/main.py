e10218cc2db7be91709e2c7e8b34e2b47b24956d
Michael Ababio <michael.ababio@contractors.mailchimp.com>
2022-09-28 12:45:09 -0400

    Bug Fix : mongodb client issue

--- src/main.py
+++ src/main.py
@@ -9,25 +9,30 @@ from tesla_stationary import TeslaStationary
 import notification
 from config import settings
 
+
+def get_db_client():
 REMOVED
-tesla_database = client['tesla']
-tesla_stationary_obj = TeslaStationary(tesla_database)
+    return client['tesla']
 
 
-def update_gps_saved(lat,lon):
+db_client = get_db_client()
+tesla_stationary_obj = TeslaStationary(db_client)
+
+
+def update_gps_saved(lat, lon):
     myquery = {"_id": 'current'}
     est = timezone('US/Eastern')
     new_values = {"$set": {"lat": lat, "lon": lon, "timestamp": str(datetime.now(est))}}
-    tesla_database['tesla_location'].update_one(myquery, new_values)
+    db_client['tesla_location'].update_one(myquery, new_values)
 
 
 def save_location(lat, lon):
     try:
-        return_saved_location = tesla_database['tesla_location'].find_one({'_id': 'current'})
+        return_saved_location = db_client['tesla_location'].find_one({'_id': 'current'})
         lat_current_saved = return_saved_location['lat']
         lon_current_saved = return_saved_location['lon']
         if lat == lat_current_saved and lon == lon_current_saved:
-            logger.info('save_location: Current lat lon values are the same as dbmongo values')
+            logger.info('save_location: Current lat lon values are the same as mongodb values')
             if tesla_stationary_obj.is_tesla_parked_long() and not tesla_stationary_obj.is_climate_turned_on_via_automation() \
                     and not tesla_stationary_obj.climate_turned_on_via_automation_before() \
                     and tesla_stationary_obj.get_db_latlon_age() < settings['production']['max_parked_min']:
@@ -51,7 +56,7 @@ def save_location(lat, lon):
                 pass
             else:
                 tesla_stationary_obj.climate_reset_for_automation()
-            logger.info('save_location: updating latlong to dbmongo ')
+            logger.info('save_location: updating lat-long to mongodb ')
     except Exception as e:
         logger.error("save_location::: Issue saving location" + str(e))
         raise
@@ -63,11 +68,10 @@ def hello_pubsub(event, context):
         lat = json.loads(pubsub_message)['lat']
         lon = json.loads(pubsub_message)['lon']
         save_location(lat, lon)
-        logger.info("hello_pubsub::::: Attempting to save lat lon to mongdb " + str(lat) + str(lon))
+        logger.info("hello_pubsub::::: Attempting to save lat lon to mongodb " + str(lat) + str(lon))
     except Exception as e:
         logger.error('ERROR ------> ' + str(e))
 
 #
 # if __name__ == "__main__":
 #     save_location("40.670042", "-74.096599")
-
