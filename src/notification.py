fc59921967b0014ca10b67462b022cfc706c20c1
mababio <michaelkwasi@gmail.com>
2023-09-05 15:18:21 -0400

    rm'd token

--- src/notification.py
+++ src/notification.py
@@ -6,8 +6,7 @@ from config import settings
 
 @retry(logger=logger, delay=2, tries=2)
 def send_push_notification(message):
-REMOVED
-    # token = settings['production']['key']['chanify']
+    token = settings['production']['key']['chanify']
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
     req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
